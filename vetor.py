7d08fea2cedf4d4b4a5c4c90ee47b15a29b6040d
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-04-17 10:57:19 -0300

    Add files via upload

---
+++ vetor.py
@@ -0,0 +1,12 @@
+vetor = [7, 12, 0, -3, 5, 100]
+#indice  0  1   2   3  4   5
+
+print(vetor)
+print(vetor.pop(3)) #vetor.pop retorna e remove o elemento de indice i do vetor
+print(vetor)
+
+if 12 in vetor:
+	print("12 esta no vetor!")
+	
+print(vetor.index(12))
+
